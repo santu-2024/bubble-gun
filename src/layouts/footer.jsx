6dce9c486be7396adeb1706fa41b82e212957e88
Andrii Pavliuk <devdice@outlook.com>
2021-03-30 05:14:00 +0300

    Prefer React components to have a shouldComponentUpdate method

--- src/layouts/footer.jsx
+++ src/layouts/footer.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
 class Footer extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+
   render() {
     return (
       <footer className='footer'>
-        &copy; The Bubble Gun Project 2021
+        {'&copy; The Bubble Gun Project 2021'}
       </footer>
     )
   }
