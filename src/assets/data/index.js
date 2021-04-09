191ac9fffead2d1f20ec7b9cffe5c71412ed036a
cutpix <devdice@outlook.com>
2021-04-09 21:49:51 +0300

    data added

---
+++ src/assets/data/index.js
@@ -0,0 +1,31 @@
+import CoverMichova from '../images/models/pmichova.jpg'
+import CoverTaylor from '../images/models/jtaylor.jpg'
+
+export const profiles = [{
+    id: 'jtaylor',
+    name: 'Jaclyn Taylor',
+    photo: CoverTaylor
+  },
+  {
+    id: 'pmichova',
+    name: 'Patty Michova',
+    photo: CoverMichova
+  }
+]
+
+export const links = [{
+    title: 'Jaclyn Taylor',
+    href: 'http://www.brazzers.com/profile/view/id/2383/jaclyn-taylor',
+    assigned: 'jtaylor'
+  },
+  {
+    title: 'Patty Michova',
+    href: 'http://www.brazzersnetwork.com/profile/view/id/2678/patty-michova',
+    assigned: 'pmichova'
+  },
+  {
+    title: 'Patty Michova - Busty Babe Masturbate',
+    href: 'http://www.hqsluts.com/Patty+Michova+-+Busty+Babe+Masturbate-369881',
+    assigned: 'pmichova'
+  }
+]
